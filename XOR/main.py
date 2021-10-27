9087302ab3cc54463807b0777f341b575a8fcc90
diarmuidkelly <dok@branchkey.com>
2021-10-27 11:55:59 +0200

    XOR

--- XOR/main.py
+++ XOR/main.py
@@ -1,11 +1,34 @@
 from data.generate import generate_dataset
 from model.model import Network
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    dataset = generate_dataset(100)
+    dataset = generate_dataset(10000)
+    test_dataset = generate_dataset(1000)
     net = Network()
+    error = []
     for (x, y) in dataset:
-        print(net.forward(x))
+        y_hat = net.forward(x)
+        if y_hat > 1:
+            y_hat = 0
+        error.append((y - y_hat)**2)
+        adj = error[-1] * y
+        net.update_weights(adj)
+    error = np.sum(error) / len(dataset)
+    print("Train error: {}".format(error))
+    error = []
+    plots = []
+    for (x, y) in test_dataset:
+        y_hat = net.forward(x)
+        if y_hat > 1:
+            y_hat = 0
+        plots.append([y - y_hat])
+        error.append((y - y_hat)**2)
+    error = np.sum(error) / len(dataset)
+    plt.plot(plots, linewidth=0.2)
+    plt.show()
+    print("Test error: {}".format(error))
 
 
 
--- XOR/model/model.py
+++ XOR/model/model.py
@@ -1,29 +1,33 @@
 import numpy as np
 
 
-def MSE_loss(X, Y, w):
-    val = 0
-    for x, y in zip(X, Y):
-        val += y - np.dot(x, w)
-    return val / 4
-
-
 class Network:
     def __init__(self):
-        self.L1 = Layer(2)
-        self.L2 = Layer(2)
+        self.L1 = Layer(layer_width=2, input_width=1, bias=[0, -1])
 
     def forward(self, x):
         x = self.L1.forward(x)
-        out = self.L2.forward(x)
-        return out
+        return np.sum(x)
+
+    def update_weights(self, adj):
+        self.L1.update_weights(adj)
 
 
 class Layer:
-    def __init__(self, input_width=2):
+    def __init__(self, layer_width=1, input_width=1, bias=None):
+        if bias is None:
+            bias = np.zeros(layer_width)
+        else:
+            bias = np.array(bias)
+        if bias.shape[0] != layer_width:
+            raise Exception("Bias must be same shape as layer")
         self.units = []
-        for u in range(input_width):
-            self.units.append(Unit())
+        for u in range(layer_width):
+            self.units.append(Unit(input_width, bias[u]))
+
+    def update_weights(self, adj):
+        for u in self.units:
+            u.weight = u.weight + adj
 
     def forward(self, x):
         ret = []
@@ -33,9 +37,9 @@ class Layer:
 
 
 class Unit:
-    def __init__(self, input_shape=2):
-        self.weight = np.ones(input_shape)
-        self.bias = [0, -1]
+    def __init__(self, input_shape=2, bias=0):
+        self.weight = np.random.uniform(0, 1, input_shape)
+        self.bias = bias
 
     def activation(self, x):
         return self.ReLu((x * self.weight) + self.bias)
